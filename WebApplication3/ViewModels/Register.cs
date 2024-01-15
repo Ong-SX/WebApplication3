812857505ebeebda5a46b918a8572b398d737bd1
ONG SI XIAN <223849J@mymail.nyp.edu.sg>
2024-01-15 11:29:36 +0800

    Add project files.

---
+++ WebApplication3/ViewModels/Register.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace WebApplication3.ViewModels
+{
+    public class Register
+    {
+		[Required]
+		public string FullName { get; set; }
+
+		[Required]
+		public string CreditCard { get; set; }
+
+		[Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirmation password does not match")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
